e2f815f8cadaf0a6333c59b40690fa95c126488c
Bjorven <bjorn.bergqvist.wingren@hyhs.se>
2018-01-04 10:21:30 +0100

    Strukturella ändringar och ökad funktionallitet i kassa-programmet. Framförallt ändringar kring hur qty hanteras. Samt hur informationen packas och skickas mellan klasserna.

---
+++ CashierClasses/CashierClasses/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierClasses
+{
+    public class CartItem
+    {
+        
+        public string ItemId { get; set; }
+
+        public string CartId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public System.DateTime DateCreated { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+    }
+}
---
+++ CashierClasses/CashierClasses/ProductGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierClasses
+{
+    public class ProductGroup
+    {
+
+        public int PGId { get; set; }
+
+        public string PGName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
